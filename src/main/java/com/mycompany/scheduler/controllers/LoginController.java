399c79b0d05ba6fef4cfaf95a87ad6d3327ecf4f
Joel <joelharris@zapatatechnology.com>
2018-01-20 20:21:07 -0500

    blah

---
+++ src/main/java/com/mycompany/scheduler/controllers/LoginController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.scheduler.controllers;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author jdharri
+ */
+public class LoginController {
+
+    @FXML
+    private Label loginLable;
+    @FXML
+    private TextField loginUsername;
+    @FXML
+    private TextField loginPassword;
+
+    public void Login(ActionEvent event) throws Exception {
+     
+
+    
+
+        System.out.println("here");
+        System.out.println("username entered:" + loginUsername.getText());
+        System.out.println("Password: " + loginPassword.getText());
+        System.out.println("label: " + loginLable.getText());
+        System.out.println("localdate: " + LocalDateTime.now());
+        Locale currentLocale = Locale.getDefault();
+        System.out.println(currentLocale.getDisplayLanguage());
+        System.out.println(currentLocale.getCountry());
+        currentLocale = Locale.CHINA;
+        System.out.println(currentLocale.getDisplayLanguage());
+        System.out.println(currentLocale.getCountry());
+      
+        if (loginUsername.getText().equals("jdharri") && loginPassword.getText().equals("pass")) {
+            loginLable.setText("Login Success");
+
+        } else {
+            loginLable.setText("Login Failed");
+        }
+    }
+}
