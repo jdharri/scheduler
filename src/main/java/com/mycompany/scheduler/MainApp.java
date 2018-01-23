73abf779d4f88843d7b0104a85430337b3751a6f
Joel Harris <jdharri@gmail.com>
2018-01-22 23:28:53 -0500

    trying to get springboot working

--- src/main/java/com/mycompany/scheduler/MainApp.java
+++ src/main/java/com/mycompany/scheduler/MainApp.java
@@ -1,7 +1,6 @@
 package com.mycompany.scheduler;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -38,7 +37,7 @@ public class MainApp extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
