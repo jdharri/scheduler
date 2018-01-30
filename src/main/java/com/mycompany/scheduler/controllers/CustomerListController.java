b73ffbf12e2143ef2780fe1ebeb4515cf8712ed0
Joel <joelharris@zapatatechnology.com>
2018-01-29 22:59:43 -0500

    blah

--- src/main/java/com/mycompany/scheduler/controllers/CustomerListController.java
+++ src/main/java/com/mycompany/scheduler/controllers/CustomerListController.java
@@ -5,13 +5,21 @@
  */
 package com.mycompany.scheduler.controllers;
 
+import com.mycompany.scheduler.model.Customer;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 /**
  * FXML Controller class
@@ -21,15 +29,42 @@ import javafx.scene.control.ListView;
 public class CustomerListController implements Initializable {
 
     @FXML
-    private ListView<String> missionsList;
-    ObservableList<String> missions = FXCollections.observableArrayList("Apollo", "Shuttle", "Skylab");
+    private ListView<Customer> customerList;
+    ObservableList<Customer> customers = FXCollections.observableArrayList();
+    SessionFactory fac;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            fac = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+        Session session = fac.openSession();
+        session.beginTransaction();
+        List<Customer> customerListResults = session.createQuery("from customer").list();
+      customers.addAll(customerListResults);
+        customerList.setCellFactory(customer -> new ListCell<Customer>(){
+           @Override
+           protected void updateItem(Customer cust, boolean empty){
+               super.updateItem(cust, empty);
+               if(empty || cust == null || cust.getCustomerName() == null){
+                   setText(null);
+               }else{
+                   setText(cust.getCustomerName());
+               }
+           }
+        });
+     
+     
+
     }
 
 }
